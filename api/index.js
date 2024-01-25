7339b16a3cf3a1f3d64daa0713b55beb425216bc
ProCodeRed <yadav.vipinkumar@outlook.com>
2024-01-26 02:53:25 +0530

    added db connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import expres from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.DB_URI).then(
+    () => {
+        console.log("db is connected")
+    }
+).catch((err) => {
+    console.log("db error", err)
+})
 
 const app = expres();
+
 
 app.listen(3000, () => {
     console.log("server is running on port 3000")
