5718918374e00cf67bfb960567dcf3a2c858c564
ProCodeRed <yadav.vipinkumar@outlook.com>
2024-01-27 01:29:58 +0530

    added global store for states

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,11 +1,14 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 const SignInPage = () => {
   const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading, error:errorMessage} = useSelector(state => state.user)
+  
+  const dispatch = useDispatch();
   const navigate = useNavigate()
 
   const handleChange = (e) => {
@@ -16,12 +19,11 @@ const SignInPage = () => {
     e.preventDefault();
 
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill all the fields given.')
+      dispatch(signInFailure('Please fill all the fields given.'))
     }
 
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -30,17 +32,17 @@ const SignInPage = () => {
       const data = await res.json();
 
       if(data.success === false){
-        return setErrorMessage(data.message = "This user already exits. Please Sign in.")
+        dispatch(signInFailure(data.message))
       }
-      setLoading(false)
 
       // if signed up successfully navigate to sign in page
+      
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false)
+      dispatch(signInFailure(error.message))
     }
   }
 
